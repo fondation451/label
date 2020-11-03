151320f43dac2c8ce2a41148b743a15590a9d7a5
Nicolas Assouad <nicolas@assouad.com>
2020-11-03 11:05:08 +0100

    flatten button folder

--- packages/generic/client/src/components/generic/materialUI/index.ts
+++ packages/generic/client/src/components/generic/materialUI/index.ts
@@ -1,11 +1,12 @@
-import { Button, SwitchButton } from './buttons';
 import { Accordion } from './Accordion';
+import { Button } from './Button';
 import { Checkbox } from './Checkbox';
 import { Dropdown } from './Dropdown';
 import { Icon } from './Icon';
 import { LayoutGrid } from './LayoutGrid';
 import { MenuBar } from './MenuBar';
 import { SvgImage } from './SvgImage';
+import { SwitchButton } from './SwitchButton';
 import { Text } from './Text';
 import { TextInput } from './TextInput';
 import { TextInputLarge } from './TextInputLarge';
